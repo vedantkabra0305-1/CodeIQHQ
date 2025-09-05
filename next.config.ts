223d8d316723eb0c5fa5645905784d6d20a24184
vedantkabra0305-1 <vedantkabra0305@gmail.com>
2025-09-05 13:54:00 +0530

    Add Pexels hostname to Next.js image configuration

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.postimg.cc',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
