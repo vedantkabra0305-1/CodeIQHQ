40eb32575ff323aa39ad163447b8a32ab1e165a9
vedantkabra0305-1 <vedantkabra0305@gmail.com>
2025-09-05 18:02:02 +0530

    Slightly reduce logo size in header

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -10,7 +10,7 @@ export function Logo({ className }: { className?: string }) {
         alt="CodeIQ"
         width={120}
         height={40}
-        className="h-8 w-auto"
+        className="h-7 w-auto"
       />
     </Link>
   );
