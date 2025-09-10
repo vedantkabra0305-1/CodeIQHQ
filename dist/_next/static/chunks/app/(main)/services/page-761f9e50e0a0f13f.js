bfbeb7093b7588fb68e87457de75313f60484c18
vedantkabra0305-1 <vedantkabra0305@gmail.com>
2025-09-10 21:37:01 +0530

    Added package.json

--- dist/_next/static/chunks/app/(main)/services/page-761f9e50e0a0f13f.js
+++
@@ -1,1 +0,0 @@
-(self["webpackChunk_N_E"]=self["webpackChunk_N_E"]||[]).push([[112],{7813:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,6874,23));Promise.resolve().then(n.t.bind(n,3063,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[63,874,441,684,358],()=>s(7813));var n=e.O();_N_E=n}]);
